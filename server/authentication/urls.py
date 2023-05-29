135db033501d006ff57b3318882540ff3db33cff
vunk.lai <vunk.lai@gmail.com>
2023-05-30 00:16:04 +0800

    feat: initial two-factor and qr-code views

--- server/authentication/urls.py
+++ server/authentication/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("register", views.register),
     path("sign_in", views.sign_in),
     path("sign_out", views.sign_out),
-    path("two_factor", views.sign_out),
+    path("two_factor", views.two_factor),
+    path("two_factor/qr_code", views.qr_code),
 ]
--- server/authentication/views.py
+++ server/authentication/views.py
@@ -16,31 +16,34 @@ def register(request: HttpRequest) -> HttpResponse:
         username = request.POST["username"]
         password = request.POST["password"]
         User.objects.create_user(username, None, password)
-        # todo: error handler
-        return redirect("/auth/login")
+        return redirect("/auth/sign_in")
     return HttpResponse("", status=HTTPStatus.METHOD_NOT_ALLOWED)
 
 
 def sign_in(request: HttpRequest) -> HttpResponse:
     if request.method == "GET":
-        return render(request, "authentication/login.html")
+        if request.user.is_authenticated:
+            return redirect("/auth/two_factor")
+        return render(request, "authentication/sign_in.html")
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(username=username, password=password)
         if user:
             login(request, user)
-            # todo: redirect 2fa page
-            return redirect("/auth/2fa")
-        # todo: error handler
-        return render(request, "authentication/login.html")
+            return redirect("/auth/two_factor")
+        return render(
+            request,
+            "authentication/sign_in.html",  # todo: error handler
+            status=HTTPStatus.BAD_REQUEST,
+        )
     return HttpResponse("", status=HTTPStatus.METHOD_NOT_ALLOWED)
 
 
 def sign_out(request: HttpRequest) -> HttpResponse:
     if request.user.is_authenticated:
         logout(request)
-    response = redirect("/auth/login")
+    response = redirect("/auth/sign_in")
     expired_time = timezone.localtime() - timezone.timedelta(minutes=5)
     response.set_cookie("auth_token", "", expires=expired_time.timestamp())
     return response
@@ -48,20 +51,33 @@ def sign_out(request: HttpRequest) -> HttpResponse:
 
 def two_factor(request: HttpRequest) -> HttpResponse:
     if not request.user.is_authenticated:
-        return render(request, "authentication/login.html")
+        return redirect("/auth/sign_in")
+
     if request.method == "GET":
-        # todo: QR-code view
-        return HttpResponse("QR Code")
+        return render(request, "authentication/two_factor.html")
     if request.method == "POST":
         code = request.POST["code"]
         totp = pyotp.TOTP(request.user.password)
         if totp.verify(code):
             # todo: redirect to $request_uri
-            response = redirect("")
+            response = redirect("/auth/sign_in")
             # todo: Create token
             response.set_cookie(
                 "auth_token", "mock-token", httponly=True, max_age=24 * 60 * 60
             )
             return response
-        return HttpResponse("")  # todo: invalid verify
+        return HttpResponse(
+            request,
+            "authentication/two_factor.html",  # todo: invalid verify
+            status=HTTPStatus.FORBIDDEN,
+        )
+    return HttpResponse("", status=HTTPStatus.METHOD_NOT_ALLOWED)
+
+
+def qr_code(request: HttpRequest) -> HttpResponse:
+    if not request.user.is_authenticated:
+        return redirect("/auth/sign_in")
+
+    if request.method == "GET":
+        return render(request, "authentication/qr_code.html")
     return HttpResponse("", status=HTTPStatus.METHOD_NOT_ALLOWED)
